08d6686a474df62572453be5608c400c4c10b43f
KingFriday1989 <kingfriday@siosphere.com>
2022-08-21 03:02:02 -0600

    //AI and Waypoints

--- Assets/Scripts/Controllers/Actor/ActorAI.cs
+++ Assets/Scripts/Controllers/Actor/ActorAI.cs
@@ -1,15 +1,44 @@
 using UnityEngine;
 using UnityEngine.AI;
+using Controllers;
+using Helpers;
 
 namespace DoubleAgent.Controllers.Actors
 {
     public class ActorAI : Actor
     {
+        public Waypoint waypoint;
+        private Waypoint currentWaypoint;
+
         protected override void Awake()
         {
             base.Awake();
             CharacterController.enabled = false;
             ActorData.IsPlayer = false;
+            currentWaypoint = waypoint;
+            StartWaypoint();
+        }
+
+        private void Update()
+        {
+            Debug.Log(currentWaypoint);
+            if(NavMeshAgent.remainingDistance <= 0.25f)
+            {
+                currentWaypoint = waypoint.Traverse();
+                NavMeshAgent.SetDestination(currentWaypoint.position);
+            }
+        }
+
+        public void StartWaypoint()
+        {
+            NavMeshAgent.SetDestination(currentWaypoint.position);
+
+            //while (this & NavMeshAgent.remainingDistance > 0.1f)
+            //{
+            //    await Timer.WaitForFrame();
+            //}
+            //currentWaypoint = currentWaypoint.Traverse();
+            //StartWaypoint();
         }
     }
 }
--- Assets/Scripts/Views/Game/ActorHud.cs
+++ Assets/Scripts/Views/Game/ActorHud.cs
@@ -1,18 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using DoubleAgent.Data;
+using DoubleAgent.Controllers.Actors;
 
 public class ActorHud : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Actor actor;
+    public TMP_Text Score;
+    public TMP_Text Grenades;
+    public TMP_Text GameTime;
+
+    private void Start()
     {
-        
+        Grenades.text = actor.ActorData.weapon.grenNum.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Score.text =  GameData.Score.ToString();
     }
 }

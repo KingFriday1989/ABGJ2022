57e671133a3ab1fec89151b26e82988ad715d487
ess4654 <pocketbuggames@outlook.com>
2022-08-21 05:40:50 -0600

    Fixed Audio Intro Issues

--- Assets/Scripts/Controllers/Intro.cs
+++ Assets/Scripts/Controllers/Intro.cs
@@ -1,25 +1,32 @@
 ﻿using Core;
 using DoubleAgent.Data;
 using Helpers.Audio;
+using Helpers.Extensions;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Video;
 
 namespace DoubleAgent.Controllers
 {
     public sealed class Intro : Scene<Intro>
     {
         [SerializeField] private AudioClip MusicStart;
+        [SerializeField] private VideoPlayer Player;
 
         private void Start()
         {
             Constants.Init();
-            SoundManager.PlayGlobalMusic(MusicStart);
             StartCoroutine(Init());    
         }
 
         IEnumerator Init()
         {
-            yield return new WaitForSeconds(8.25f);
+            yield return new WaitUntil(() => Player.time > 0);
+            GameData.GlobalMusicChannel = SoundManager.PlayGlobalMusic(MusicStart).GetComponent<AudioSource>();
+            if (GameData.GlobalMusicChannel != null)
+                DontDestroyOnLoad(GameData.GlobalMusicChannel.gameObject);
+            //yield return new WaitForSeconds(8.25f);
+            yield return new WaitUntil(() => Player.IsFinished());
             LoadScene(1); //Main Menu
         }
     }
--- Assets/Scripts/Controllers/MainMenu.cs
+++ Assets/Scripts/Controllers/MainMenu.cs
@@ -1,21 +1,52 @@
 using Core;
+using DoubleAgent.Data;
 using Helpers;
 using Helpers.Audio;
+using Helpers.Extensions;
 using UnityEngine;
 
 namespace DoubleAgent.Controllers
 {
     public sealed class MainMenu : Scene<MainMenu>
     {
-        [SerializeField] private AudioClip StartMusic;
+        //[SerializeField] private AudioClip StartMusic;
         [SerializeField] private AudioClip LoopMusic;
+        [SerializeField] private Canvas MenuCanvas;
+        [SerializeField] private AudioSource Music;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            Music.SetActive(false);
+        }
 
         private async void Start()
         {
-            SoundManager.PlayMusicOnChannel(StartMusic, 0);
-            SoundManager.SetMusicVolumeOnChannel(0.3f, 0);
-            await Timer.WaitForSeconds(StartMusic.length);
-            SoundManager.LoopMusicOnChannel(LoopMusic, 0);
+            //SoundManager.PlayMusicOnChannel(StartMusic, 0);
+            //SoundManager.SetMusicVolumeOnChannel(0.3f, 0);
+            //SoundManager.SetVolume(0.3f);
+
+            if(GameData.GlobalMusicChannel != null)
+                GameData.GlobalMusicChannel.volume = 0.8f;
+            
+            //await Timer.WaitForSeconds(StartMusic.length);
+            await Timer.WaitWhile(() => this && GameData.GlobalMusicChannel && !GameData.GlobalMusicChannel.IsFinished());
+            if (GameData.GlobalMusicChannel != null)
+                Destroy(GameData.GlobalMusicChannel.gameObject);
+
+            Music.SetActive(true);
+
+            //!BROKEN
+            //SoundManager.LoopMusicOnChannel(LoopMusic, 0);
+            //SoundManager.SetMusicVolumeOnChannel(0.8f, 0);
+        }
+
+        public async void LoadGame()
+        {
+            MenuCanvas.enabled = false;
+            ShowLoadingScreen();
+            await Timer.WaitForFrame();
+            await LoadScene(2);
         }
     }
 }

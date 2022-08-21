dc04d32f4d3dc16a6ca092e55c2f6b71d28bc127
ess4654 <pocketbuggames@outlook.com>
2022-08-21 01:52:23 -0600

    Added Score

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -1,9 +1,27 @@
-﻿using UnityEngine;
+﻿using Data.Game;
+using System.Threading.Tasks;
+using UnityEngine;
 
 namespace DoubleAgent.Data
 {
-    public sealed class GameData
+    public sealed class GameData : GameDataBase<GameData, GameStates>
     {
         public static AudioSource GlobalMusicChannel;
+
+        public override string SaveFolder => throw new System.NotImplementedException();
+        public override string SaveExtension => throw new System.NotImplementedException();
+        public override bool CompressSaveFile => throw new System.NotImplementedException();
+
+        protected override Task<string> GetMetaData() => null;
+        protected override Task<string> GetSaveData() => null;
+        protected override Task<MetaData> LoadMetaDataFromPath<MetaData>(string path, bool compressed) => null;
+        protected override void SetGameData<SaveData>(SaveData data) { }
+
+        public static int Score;
+
+        protected override void Reset()
+        {
+            Score = 0;
+        }
     }
 }

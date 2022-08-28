ec6e2de5962a6aa4473135846eacd601cacff75f
setothegreat <seto007@gmail.com>
2022-08-27 22:12:31 -0600

    FIXED DESTRUCTION :D

--- Assets/Scripts/Controllers/Weapons/Bomb.cs
+++ Assets/Scripts/Controllers/Weapons/Bomb.cs
@@ -1,6 +1,9 @@
 using DoubleAgent.Data;
 using UnityEngine;
 using UnityEngine.Events;
+using RayFire;
+using System.Collections.Generic;
+using Helpers;
 
 namespace Controllers.Game
 {
@@ -16,6 +19,26 @@ namespace Controllers.Game
                 GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
                 rend.enabled = false;
                 _OnExplode?.Invoke();
+            }
+        }
+        public async void CheckForDestructibles()
+        {
+            var rayfirebombscript = GetComponent<RayfireBomb>();
+            var radius = rayfirebombscript.range;
+            var hitobjects = Physics.SphereCastAll(transform.position,radius,transform.forward,radius+1);
+            var rigidbodies = new List<RayfireRigid>();
+            foreach (var hitobject in hitobjects)
+            {
+                if (hitobject.collider.TryGetComponent(out RayfireRigid rigid))
+                {
+                    rigidbodies.Add(rigid);
+                }
+            }
+            await Timer.WaitForFrame();
+            for (int i = rigidbodies.Count-1; i >= 0; i--)
+            {
+                if (rigidbodies[i].demolitionType!=DemolitionType.None)
+                { rigidbodies[i].Demolish(); }
             }
         }
     }
